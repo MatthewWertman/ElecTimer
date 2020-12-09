5ce016547062e88d05491e323e4b1daf5a4030ae
MatthewWertman <mattwert@charter.net>
2020-12-09 12:40:03 -0800

    Make application more 'dev' friendly

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const {app, BrowserWindow, globalShortcut} = require("electron");
 function createWindow () {
     const win = new BrowserWindow({
         title: app.name,
-        width: 500,
-        height: 400,
+        width: 800,
+        heigt: 600,
+        //width: 300,
+        //height: 85,
         icon: "build/icon.png",
         webPreferences: {
             nodeIntegration: true
@@ -12,15 +14,20 @@ function createWindow () {
     });
 
     win.loadFile("./base/index.html");
+    win.openDevTools();
 
     //globals
     globalShortcut.register("num1", () => {
         console.log("Pressed num1");
         win.webContents.executeJavaScript("document.getElementById('startButton').click()");
     });
+    globalShortcut.register("num5", () => {
+        console.log("Pressed num5");
+        win.webContents.executeJavaScript("document.getElementById('stopButton').click()");
+    });
     globalShortcut.register("num3", () => {
         console.log("Pressed num3");
-        win.webContents.executeJavaScript("document.getElementById('stopButton').click()");
+        win.webContents.executeJavaScript("document.getElementById('resetButton').click()");
     });
 }
 
