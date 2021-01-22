dfdea8ed9669cadc38c2a91780df42b2cf04cdfa
MatthewWertman <mattwert@charter.net>
2021-01-22 11:21:21 -0800

    messing with spliting

--- render/renderer.js
+++ render/renderer.js
@@ -1,3 +1,4 @@
+/*eslint no-undef: "off"*/
 const moment = require("moment");
 require("moment-duration-format");
 
@@ -12,9 +13,55 @@ const resetBtn = document.getElementById("resetButton");
 let runningWatch;
 let isRunning = false;      //Bool if stopwatch is running
 
-function update () {
-    $time.innerHTML = moment.duration(sw.time, "milliseconds").format("hh:mm:ss.SS", {stopTrim: "s"});
+function format_time (time) {
+    return moment.duration(time, "milliseconds").format("hh:mm:ss.SS", {stopTrim: "s"});
 }
+
+function createSplit (titleText, time) {
+
+    const splitContainer = document.createElement("div");
+    const lap = document.createElement("span");
+    const card = document.createElement("span");
+
+    const splitContainerClass = document.createAttribute("id");
+    splitContainerClass.value = "splitContainer";
+    const lapClass = document.createAttribute("class");
+    lapClass.value = "split";
+
+    card.appendChild(document.createTextNode(titleText));
+
+    lap.setAttributeNode(lapClass);
+    lap.appendChild(document.createTextNode(format_time(time)));
+
+    splitContainer.appendChild(card);
+    splitContainer.appendChild(lap);
+    splitContainer.setAttributeNode(splitContainerClass);
+    document.body.insertBefore(splitContainer, $time);
+}
+
+function removeSplits () {
+    const laps = document.querySelectorAll("div");
+    laps.forEach(lap => {
+        lap.remove();
+    });
+}
+
+function update () {
+    $time.innerHTML = format_time(sw.time);
+}
+
+// Potential solution to format the time without the use of moment.
+//let hrs, mins;
+// hrs = Number(new Date(sw.time).toISOString().substr(11,2));
+// mins = Number(new Date(sw.time).toISOString().substr(14,2));
+//
+// if (hrs >= 1) {
+//     $time.innerHTML = new Date(sw.time).toISOString().substr(11,8);
+// } else if (mins >= 1) {
+//     $time.innerHTML = new Date(sw.time).toISOString().substr(14,8);
+// } else {
+//     $time.innerHTML = new Date(sw.time).toISOString().substr(17,5);
+// }
 
 function start () {
     isRunning = true;
@@ -34,11 +81,16 @@ function reset () {
     stop();
     $time.style.color = "white";
     sw.reset();
+    removeSplits();
     update();
 }
 
 startBtn.addEventListener("click", () => {
-    start();
+    if (isRunning) {
+        createSplit("Time", sw.time);
+    } else {
+        start();
+    }
 });
 stopBtn.addEventListener("click", () => {
     if (isRunning) {
